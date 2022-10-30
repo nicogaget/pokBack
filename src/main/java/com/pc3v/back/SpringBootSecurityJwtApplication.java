576d2b39697e4f5f62d48833e2c2fed326b56c6e
Nico <n.gaget69@gmail.com>
2022-10-30 17:03:03 +0100

    authentification avec jwt

---
+++ src/main/java/com/pc3v/back/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.pc3v.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pc3v/back/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.pc3v.back.repository;
+
+import java.util.Optional;
+
+import com.pc3v.back.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
